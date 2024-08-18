b4b8ccdcd188e53b470b382b658610f5f6ff7cc9
Ron Dekker <RedKenrok@gmail.com>
2024-08-18 20:14:54 +0200

    Update typing of iife wrapper;

--- .scripts/iife.ts
+++ .scripts/iife.ts
@@ -1,10 +1,8 @@
-type GenericObjectAny = { [key: string]: any }
-
 export const iife = function (
   path: Array<string>,
   data: any
 ) {
-  let subject: GenericObjectAny = window
+  let subject: Record<string, any> = window
   for (let i = 0; i < path.length - 1; i++) {
     if (typeof (subject[path[i]]) !== 'object' || !Array.isArray(subject[path[i]])) {
       subject[path[i]] = {}
