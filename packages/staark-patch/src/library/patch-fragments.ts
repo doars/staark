b01e8a3b557cf7a6d85fdc171ae8b6de39c9832f
Ron Dekker <RedKenrok@gmail.com>
2025-02-07 19:39:29 +0100

    Experimented with using document fragments;

---
+++ packages/staark-patch/src/library/patch-fragments.ts
@@ -0,0 +1,376 @@
+import {
+  arrayify,
+} from '@doars/staark-common/src/array.js'
+import {
+  childrenToNodes,
+} from '@doars/staark-common/src/element.js'
+import {
+  NodeAbstract,
+  NodeAttributes,
+  NodeAttributeListener,
+  NodeContent,
+} from '@doars/staark-common/src/node.js'
+
+export type PatchFunction = (
+  newAbstractTree: NodeContent[] | NodeContent,
+) => void
+
+const MATCH_CAPITALS = /[A-Z]+(?![a-z])|[A-Z]/g
+const HYPHENATE = (
+  part: string,
+  offset: number,
+) => (offset ? '-' : '') + part
+
+const updateAttributes = (
+  element: Element,
+  newAttributes?: NodeAttributes,
+  oldAttributes?: NodeAttributes,
+): void => {
+  if (newAttributes) {
+    for (const name in newAttributes) {
+      let value = newAttributes[name]
+      if (value) {
+        const type = typeof (value)
+        if (type === 'function') {
+          if (
+            oldAttributes
+            && oldAttributes[name]
+          ) {
+            if (oldAttributes[name] === value) {
+              continue
+            } else {
+              element.removeEventListener(
+                name,
+                oldAttributes[name] as NodeAttributeListener,
+              )
+            }
+          }
+
+          element.addEventListener(
+            name,
+            value as NodeAttributeListener,
+          )
+        } else {
+          if (name === 'class') {
+            if (typeof (value) === 'object') {
+              if (Array.isArray(value)) {
+                value = value.join(' ')
+              } else {
+                let classNames: string = ''
+                for (const className in value) {
+                  if (value[className]) {
+                    classNames += ' ' + className
+                  }
+                }
+                value = classNames
+              }
+            }
+            element.className = value as string
+          } else if (
+            name === 'style'
+            && typeof (value) === 'object'
+          ) {
+            // Apply updated styles.
+            for (let styleName in value) {
+              let styleValue = (value as Record<string, boolean | string | null | undefined | number | (boolean | string | number)[]>)[styleName]
+
+              // Convert to kebab case.
+              styleName = styleName
+                .replace(MATCH_CAPITALS, HYPHENATE)
+                .toLowerCase()
+
+              if (Array.isArray(styleValue)) {
+                styleValue = styleValue.join(' ')
+              }
+
+              (element as HTMLElement).style.setProperty(
+                styleName,
+                styleValue as string,
+              )
+            }
+
+            // Remove old styles.
+            if (
+              oldAttributes
+              && oldAttributes[name]
+              && typeof (oldAttributes[name]) === 'object'
+              && !Array.isArray(oldAttributes[name])
+            ) {
+              for (let styleName in oldAttributes[name]) {
+                if (!(styleName in value)) {
+                  styleName = styleName
+                    .replace(MATCH_CAPITALS, HYPHENATE)
+                    .toLowerCase();
+
+                  (element as HTMLElement).style.removeProperty(
+                    styleName,
+                  )
+                }
+              }
+            }
+          } else {
+            // Ensure it is of type string.
+            if (type === 'boolean') {
+              if (!value) {
+                element.removeAttribute(name)
+                continue
+              }
+              value = 'true'
+            } else if (type !== 'string') {
+              value = value.toString()
+            }
+
+            if (
+              name === 'value'
+              && (element as HTMLInputElement).value !== value
+            ) {
+              // Update value separately as well.
+              (element as HTMLInputElement).value = value as string
+              // Don't dispatch a change event, the re-rendering should update everything: element.dispatchEvent(new Event('change'))
+            } else if (name === 'checked') {
+              (element as HTMLInputElement).checked = newAttributes[name] as boolean
+              // Don't dispatch a change event, the re-rendering should update everything: element.dispatchEvent(new Event('change'))
+            }
+
+            element.setAttribute(name, (value as string))
+          }
+        }
+      }
+    }
+  }
+
+  // Cleanup old attributes.
+  if (oldAttributes) {
+    for (const name in oldAttributes) {
+      if (
+        !newAttributes
+        || !newAttributes[name]
+      ) {
+        if (typeof (oldAttributes[name]) === 'function') {
+          element.removeEventListener(
+            name,
+            oldAttributes[name] as NodeAttributeListener,
+          )
+        } else if (name === 'class') {
+          element.className = ''
+        } else if (name === 'style') {
+          (((element as HTMLElement).style as unknown) as string) = ''
+        } else {
+          if (name === 'value') {
+            // Reset value separately.
+            (element as HTMLInputElement).value = ''
+            // Don't dispatch the input change event, the rerendering should update everything: element.dispatchEvent(new Event('change'))
+          } else if (name === 'checked') {
+            (element as HTMLInputElement).checked = false
+          }
+          element.removeAttribute(name)
+        }
+      }
+    }
+  }
+}
+
+const insertFragment = (
+  element: Element,
+  fragment: DocumentFragment,
+  position: InsertPosition | null,
+  referenceNode: Node | null
+) => {
+  switch (position) {
+    case 'afterbegin':
+      element.insertBefore(fragment, element.firstChild)
+      break
+    case 'beforeend':
+      element.appendChild(fragment)
+      break
+
+    default:
+      // case 'beforebegin':
+      if (referenceNode) {
+        element.insertBefore(fragment, referenceNode)
+      }
+      break
+  }
+}
+
+export const prepare = (
+  rootElement: HTMLElement | Element | string,
+  oldAbstractTree?: NodeContent[] | string,
+): PatchFunction => {
+  const updateElementTree = (
+    element: Element,
+    newChildAbstracts?: NodeContent[],
+    oldChildAbstracts?: NodeContent[],
+  ): void => {
+    let newIndex = 0
+    let newCount = 0
+    if (newChildAbstracts) {
+      let currentFragment: DocumentFragment | null = null
+      let currentPosition: InsertPosition | null = null
+      let currentReferenceNode: Node | null = null
+
+      for (; newIndex < newChildAbstracts.length; newIndex++) {
+        const newAbstract = newChildAbstracts[newIndex]
+
+        // Try to find the matching old abstract.
+        let matched = false
+        if (oldChildAbstracts) {
+          for (let oldIndex = newIndex - newCount; oldIndex < oldChildAbstracts.length; oldIndex++) {
+            const oldAbstract = oldChildAbstracts[oldIndex];
+            if (
+              (
+                (oldAbstract as NodeAbstract).t
+                && (newAbstract as NodeAbstract).t === (oldAbstract as NodeAbstract).t
+              )
+              || (
+                !(oldAbstract as NodeAbstract).t
+                && !(newAbstract as NodeAbstract).t
+              )
+            ) {
+              matched = true
+
+              if (newIndex !== (oldIndex + newCount)) {
+                // Move node in dom.
+                element.insertBefore(
+                  element.childNodes[oldIndex + newCount],
+                  element.childNodes[newIndex],
+                )
+                // Move node in abstract tree.
+                oldChildAbstracts.splice(
+                  newIndex - newCount,
+                  0,
+                  ...oldChildAbstracts.splice(
+                    oldIndex,
+                    1,
+                  )
+                )
+              }
+
+              if ((newAbstract as NodeAbstract).t) {
+                updateAttributes(
+                  (element.childNodes[newIndex] as Element),
+                  (newAbstract as NodeAbstract).a,
+                  (oldAbstract as NodeAbstract).a,
+                )
+                updateElementTree(
+                  (element.childNodes[newIndex] as Element),
+                  (newAbstract as NodeAbstract).c,
+                  (oldAbstract as NodeAbstract).c,
+                )
+              } else if (oldAbstract !== newAbstract) {
+                element.childNodes[newIndex].textContent = newAbstract as string
+              }
+              break
+            }
+          }
+        }
+
+        if (!matched) {
+          let childElement: Element | Text
+          if ((newAbstract as NodeAbstract).t) {
+            childElement = document.createElement(
+              (newAbstract as NodeAbstract).t
+            )
+
+            if ((newAbstract as NodeAbstract).a) {
+              updateAttributes(
+                childElement,
+                (newAbstract as NodeAbstract).a,
+              )
+            }
+            if ((newAbstract as NodeAbstract).c) {
+              updateElementTree(
+                childElement,
+                (newAbstract as NodeAbstract).c,
+              )
+            }
+          } else {
+            childElement = document.createTextNode(newAbstract as string)
+          }
+
+          let position: InsertPosition | null = null
+          let referenceNode: Node | null = null
+          if (newIndex === 0) {
+            position = 'afterbegin'
+          } else if ((oldChildAbstracts?.length ?? 0) + newCount > newIndex) {
+            referenceNode = element.childNodes[newIndex]
+          } else {
+            position = 'beforeend'
+          }
+
+          if (
+            position === currentPosition
+            && referenceNode === currentReferenceNode
+          ) {
+            currentFragment!.appendChild(childElement)
+          } else {
+            if (currentFragment) {
+              insertFragment(
+                element,
+                currentFragment,
+                currentPosition,
+                currentReferenceNode
+              )
+            }
+
+            currentFragment = document.createDocumentFragment()
+            currentPosition = position
+            currentReferenceNode = referenceNode
+            currentFragment.appendChild(childElement)
+          }
+
+          newCount++
+        }
+      }
+
+      if (currentFragment) {
+        insertFragment(
+          element,
+          currentFragment,
+          currentPosition,
+          currentReferenceNode
+        )
+      }
+    }
+
+    // Remove old elements.
+    const elementLength = (oldChildAbstracts?.length ?? 0) + newCount
+    if (elementLength >= newIndex) {
+      for (let i = elementLength - 1; i >= newIndex; i--) {
+        element.childNodes[i].remove()
+      }
+    }
+  }
+
+  const _rootElement = (
+    typeof (rootElement) === 'string'
+      ? (
+        document.querySelector(rootElement)
+        || document.body.appendChild(
+          document.createElement('div')
+        )
+      )
+      : rootElement
+  )
+
+  if (typeof (oldAbstractTree) === 'string') {
+    try {
+      oldAbstractTree = JSON.parse(oldAbstractTree) as NodeContent[]
+    } catch (error) {
+      oldAbstractTree = undefined
+    }
+  }
+  oldAbstractTree ??= childrenToNodes(_rootElement)
+
+  return (
+    newAbstractTree: NodeContent[] | NodeContent,
+  ): void => {
+    newAbstractTree = arrayify(newAbstractTree)
+    updateElementTree(
+      _rootElement,
+      newAbstractTree,
+      oldAbstractTree as NodeContent[],
+    )
+    oldAbstractTree = newAbstractTree
+  }
+}
